87fdf76fdaa30ec42271ce159ff2da7eb917e10a
Binh Nguyen <tug21976@temple.edu>
2018-10-17 09:45:02 -0400

    Use ListView instead of Spinner
    A ListView is now used to display the list of colors instead of a Spinner.

--- app/src/main/java/edu/temple/lab3/PaletteActivity.java
+++ app/src/main/java/edu/temple/lab3/PaletteActivity.java
@@ -1,6 +1,5 @@
 package edu.temple.lab3;
 
-import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -8,13 +7,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Spinner;
+import android.widget.ListView;
 
 public class PaletteActivity extends AppCompatActivity {
 
-    private boolean isIntentional = false;
-
-    Spinner spinner;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,23 +22,16 @@ public class PaletteActivity extends AppCompatActivity {
         Resources res = getResources();
         String[] colors = res.getStringArray(R.array.colors_array);
 
-        spinner = findViewById(R.id.spinner);
-        spinner.setAdapter(new ColorAdapter(this, colors));
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        listView = findViewById(R.id.listView);
+        listView.setAdapter(new ColorAdapter(this, colors));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if (isIntentional) {
-                    FragmentManager fragmentManager = getSupportFragmentManager();
-                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                    CanvasFragment canvasFragment = CanvasFragment.newInstance(ColorAdapter.COLORS[position]);
-                    fragmentTransaction.replace(R.id.container, canvasFragment).addToBackStack(null).commit();
-                } else {
-                    isIntentional = true;
-                }
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                CanvasFragment canvasFragment = CanvasFragment.newInstance(ColorAdapter.COLORS[position]);
+                fragmentTransaction.replace(R.id.container, canvasFragment).addToBackStack(null).commit();
             }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) { }
         });
     }
 
